f9fbdd8eb8b99666072d117828cbbd4b41eee47e
m@g <mattghall@live.com>
2018-01-06 16:56:11 -0800

    Hard code defaults and try to get transit options for morning commute

--- directions.js
+++ directions.js
@@ -1,9 +1,10 @@
 function initMap() {
         var directionsDisplay = new google.maps.DirectionsRenderer;
         var directionsService = new google.maps.DirectionsService;
+        var downtownSeattle = new google.maps.LatLng(47.602384, -122.332895)
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 7,
-          center: {lat: 41.85, lng: -87.65}
+          zoom: 12,
+          center: downtownSeattle
         });
         directionsDisplay.setMap(map);
         directionsDisplay.setPanel(document.getElementById('right-panel'));
@@ -15,18 +16,29 @@ function initMap() {
         var onChangeHandler = function() {
           calculateAndDisplayRoute(directionsService, directionsDisplay);
         };
-        document.getElementById('start').addEventListener('change', onChangeHandler);
-        document.getElementById('end').addEventListener('change', onChangeHandler);
+        //document.getElementById('start').addEventListener('change', onChangeHandler);
+        //document.getElementById('end').addEventListener('change', onChangeHandler);
+        document.getElementById('submit').addEventListener('click', onChangeHandler);
       }
 
       function calculateAndDisplayRoute(directionsService, directionsDisplay) {
         var start = document.getElementById('start').value;
         var end = document.getElementById('end').value;
-        directionsService.route({
+        var arrival = new Date('January 11 2017');
+        arrival.setHours(8);
+        arrival.setMinutes(30);
+        var transitOptions = {
+          arrivalTime: arrival,
+          routingPreference: 'LESS_WALKING'
+        };
+        var request = {
           origin: start,
           destination: end,
-          travelMode: 'DRIVING'
-        }, function(response, status) {
+          travelMode: 'TRANSIT',
+          transitOptions: transitOptions
+        };
+
+        directionsService.route(request, function(response, status) {
           if (status === 'OK') {
             directionsDisplay.setDirections(response);
           } else {
